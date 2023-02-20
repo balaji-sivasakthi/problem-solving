2fc146f5728d09e14cea5a5403332218669f6aba
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2023-02-20 23:35:59 +0530

    bubble sort

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Sort s = new Sort(new int[]{2,3,5,4,9});
 //        s.insertionSort();
-        s.selectionSort();
+//        s.selectionSort();
+        s.bubbleSort();
         s.printArr();
+
     }
 }
 
--- src/com/latrosoft/Sort/Sort.java
+++ src/com/latrosoft/Sort/Sort.java
@@ -31,7 +31,18 @@ public class Sort {
         }
     }
     public void bubbleSort(){
-
+        for(int i=0;i<arr.length;i++){
+            int flag =0;
+            for(int j=1;j<arr.length-1;j++){
+                    if(arr[j-1]>arr[j]){
+                        int temp = arr[j-1];
+                        arr[j-1]=arr[j];
+                        arr[j]= temp;
+                        flag=1;
+                    }
+            }
+            if(flag==0) break;
+        }
     }
     public void mergeSort(){
 
